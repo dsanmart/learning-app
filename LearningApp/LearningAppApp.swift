c6620c4a8b9221da0a7389413370bf0265427b06
dsanmart <diegosancarrion@gmail.com>
2021-11-02 01:06:08 +0100

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Diego Sanmartin on 02/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
